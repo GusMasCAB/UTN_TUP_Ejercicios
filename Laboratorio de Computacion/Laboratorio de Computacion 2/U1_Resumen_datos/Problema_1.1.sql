9effc73074d397ee6162453fb307621e9392139f
Gustavo Martin Masera <gustavomasera491@gmail.com>
2023-09-03 01:35:48 -0300

    Problema 1.1 LAB

---
+++ Laboratorio de Computacion/Laboratorio de Computacion 2/U1_Resumen_datos/Problema_1.1.sql
@@ -0,0 +1,85 @@
+﻿﻿/*Problema 1.1: Consultas Sumarias*/
+--7. Se quiere saber la cantidad de ventas que hizo el vendedor de código 3.
+select 'Vendedor cod 3' Vendedor,count(*) 'Cantidad de ventas'
+from facturas
+where cod_vendedor=3
+
+--8. ¿Cuál fue la fecha de la primera y última venta que se realizó en este 
+--negocio?
+select min(fecha) '1ra Venta', max(fecha) 'Ultima Venta'
+from facturas
+
+--9. Mostrar la siguiente información respecto a la factura nro.: 450: cantidad 
+--total de unidades vendidas, la cantidad de artículos diferentes vendidos y 
+--el importe total.
+select sum(cantidad) 'Cantidad unidades vendidas', 
+count(distinct cod_articulo) 'Cantidad de articulos',
+sum(pre_unitario*cantidad) 'Importe total'
+from detalle_facturas
+where nro_factura=450
+
+--10.¿Cuál fue la cantidad total de unidades vendidas, importe total y el importe 
+--promedio para vendedores cuyos nombres comienzan con letras que van 
+--de la d a la l?
+select sum(cantidad) 'Cantidad unidades vendidas', 
+sum(pre_unitario*cantidad) 'Importe total',
+sum(pre_unitario*cantidad)/(count(distinct d.nro_factura)) 'Importe promedio'
+from detalle_facturas d
+join facturas f on f.nro_factura=d.nro_factura
+join vendedores v on v.cod_vendedor=f.cod_vendedor
+where nom_vendedor like '[d-l]%'
+
+--11.Se quiere saber el importe total vendido, el promedio del importe vendido y 
+--la cantidad total de artículos vendidos para el cliente Roque Paez.
+select sum(pre_unitario*cantidad) 'Importe total',
+sum(pre_unitario*cantidad)/(count(distinct d.nro_factura)) 'Importe promedio',
+sum(cantidad) 'Cantidad articulos vendidos'
+from detalle_facturas d
+join facturas f on f.nro_factura=d.nro_factura
+join clientes c on c.cod_cliente=f.cod_cliente
+where nom_cliente='roque'
+and ape_cliente='paez'
+
+--12.Mostrar la fecha de la primera venta, la cantidad total vendida y el importe 
+--total vendido para los artículos que empiecen con C.
+select min(fecha) '1ra Venta',sum(cantidad) 'Cantidad total',
+sum(d.pre_unitario*cantidad) 'Importe total'
+from detalle_facturas d
+join articulos a on a.cod_articulo=d.cod_articulo
+join facturas f on f.nro_factura=d.nro_factura
+where a.descripcion like 'c%'
+
+--13.Se quiere saber la cantidad total de artículos vendidos y el importe total 
+--vendido para el periodo del 15/06/2011 al 15/06/2017.
+select count(distinct cod_articulo) 'Articulos vendidos',
+sum(cantidad) 'Unidades vendidas', sum(pre_unitario*cantidad) 'Importe total' 
+from detalle_facturas d
+join facturas f on f.nro_factura=d.nro_factura 
+where fecha between '15/06/2011' and '15/06/2017'
+
+--14.Se quiere saber la cantidad de veces y la última vez que vino el cliente de 
+--apellido Abarca y cuánto gastó en total.
+select count(distinct d.nro_factura) 'Cantidad de veces',
+max(fecha) 'Ultima vez', sum(pre_unitario*cantidad) 'Gasto total'
+from detalle_facturas d
+join facturas f on f.nro_factura=d.nro_factura
+join clientes c on c.cod_cliente=f.cod_cliente
+where ape_cliente='abarca'
+
+--15.Mostrar el importe total y el promedio del importe para los clientes cuya 
+--dirección de mail es conocida.
+select sum(pre_unitario*cantidad) 'Importe total',
+sum(pre_unitario*cantidad)/count(distinct d.nro_factura) 'Promedio importe total'
+from detalle_facturas d
+join facturas f on f.nro_factura=d.nro_factura
+join clientes c on c.cod_cliente=f.cod_cliente
+where  c.[e-mail] is not null
+
+--16.Obtener la siguiente información: el importe total vendido y el importe 
+--promedio vendido para números de factura que no sean los siguientes: 13, 
+--5, 17, 33, 24
+select sum(pre_unitario*cantidad) 'Importe total',
+sum(pre_unitario*cantidad)/count(distinct d.nro_factura) 'Importe promedio'
+from detalle_facturas d
+join facturas f on f.nro_factura=d.nro_factura
+where d.nro_factura not in (13,5,17,33,24)
---
+++ Programacion/Programacion_1/ABMPersonas2/frmPersona.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;              //Clases de ADO.NET
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;    //Proveedor de ADO.NET
+using System.Net;
+
+namespace ABMPersonas
+{
+    //primera parte del ejercicio
+    ///segunda parte del ejercicio
+    public partial class frmPersona : Form
+    {
+        bool nuevo = false;  // bandera para evitar errores por PK. (true)estamos ingresando datos de una nueva persona
+                                                                  //(false)estamos editando los datos existentes de una persona
+        SqlConnection conexion = new SqlConnection();
+        SqlCommand comando = new SqlCommand();
+        SqlDataReader lector;  /// el Data Reader no posee constructor
+        /// SqlConnection nos permite establecer la conexion con el proveedor    
+        /// SqlCommand nos permite ejecutar un comando Select, Insert, Update, Delete <summary>
+        /// DataReader y DataTable que son componentes que van a servir para alojar datos
+      
+        public frmPersona()  // Constructor de frmPersona
+        {
+            InitializeComponent();
+        }
+        //Evento load tiene lugar cuando se carga el formulario
+        //Es el evento por defecto del formulario
+        ///para conectarnos a la base de datos comenzamos en el load para tener los combo y listas cargados
+        private void frmPersona_Load(object sender, EventArgs e)
+        {
+            Habilitar(false);
+            /// ConnectionString es una propiedad de tipo string que obtiene o establece la cadena de conexion para conectarnos a una base de datos
+            /// @ ----> Interpreta que lo que va despues es cadena de texto. Esto evita errores con caracteres especiales ejemplo \
+            conexion.ConnectionString = @"Data Source=.\sqlexpress;Initial Catalog=TUPPI;Integrated Security=True";
+            conexion.Open(); /// abrimos la conexion
+            /// Le decimos a traves de que conexion va a ejecutar los comandos 
+            comando.Connection = conexion;
+            /// Le indicamos el tipo de comando. En este caso es un comando de tipo texto de SQL. Tamnien hay de tipo procedimiento almacenado
+            comando.CommandType = CommandType.Text;
+            ///le pasamos la instruccion de TRANSACT-SQL
+            comando.CommandText = "SELECT * FROM tipo_documento";
+            /// Al resultado de la consulta decidimos si ponerlo en un DataReader o un DataTable. 
+            /// Para los ComboBox es más practico ponerlo en un DataTable
+            
+            DataTable tabla = new DataTable();
+            ///Método Load() de la clase DataTable que rellena la instancia(tabla) de la clase DataTable con el DataReader pasado como parametro
+            ///Metodo ExecuteReader() de la clase Comando. Ejecuta el comado y devuelve un objeto tipo DataReader, es decir las tablas resultado de la consulta
+            tabla.Load(comando.ExecuteReader()); ///Todos los Select lo ejecutamos con ExecuteReader()
+
+            conexion.Close(); /// cerramos la conexión
+
+            ///Una vez desconectados le pasamos al ComboBox el DataTable
+            /// DataSource propiedad que establece la fuente de datos del control
+            cboTipoDocumento.DataSource = tabla;
+            ///DisplayMember indicamos lo que se quiere ver en el Combo
+            cboTipoDocumento.DisplayMember = "n_tipo_documento";
+            /// ValueMember indicamos el valor que va a tomar. Acordarse ValueMember != IndexMember
+            cboTipoDocumento.ValueMember = "id_tipo_documento";
+
+            conexion.ConnectionString = @"Data Source=.\sqlexpress;Initial Catalog=TUPPI;Integrated Security=True";
+            conexion.Open(); 
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = "SELECT * FROM estado_civil";
+ 
+            DataTable tabla1 = new DataTable();
+            tabla1.Load(comando.ExecuteReader()); 
+            conexion.Close(); 
+
+            cboEstadoCivil.DataSource = tabla1;
+            cboEstadoCivil.DisplayMember = "n_estado_civil";
+            cboEstadoCivil.ValueMember = "id_estado_civil";
+            cboEstadoCivil.SelectedIndex = 0;
+        }
+
+        //Metodo para habilitar/deshabilitar los controles del formulario
+        private void Habilitar(bool x)
+        {
+            // TextBox: son una clase del name Space System.Windows.Forms
+            txtApellido.Enabled = x;   // Enabled es una propiedad de los controles
+            txtNombres.Enabled = x;    // de tipo booleano. Establece si el control
+            txtDocumento.Enabled = x;  // puede responder a la interaccion del usuario
+
+            // ComboBox:  son una clase del name Space System.Windows.Forms
+            cboTipoDocumento.Enabled = x;
+            cboEstadoCivil.Enabled = x;
+
+            // RadioButton y CheckBox: son una clase del name Space System.Windows.Forms
+            rbtMasculino.Enabled = x;
+            rbtFemenino.Enabled = x;
+            chkFallecio.Enabled = x;
+
+            // Button: son una clase del name Space System.Windows.Forms
+            btnGrabar.Enabled = x;
+            btnCancelar.Enabled = x;
+            
+            //Los proximos son diferentes !x debido a que al cargar el frmPersona
+            // deben estar habilitados
+            btnNuevo.Enabled = !x;
+            btnEditar.Enabled = !x;
+            btnBorrar.Enabled = !x;
+            btnSalir.Enabled = !x;
+
+            // ListBox: son una clase del name Space System.Windows.Forms
+            lstPersonas.Enabled = !x;
+        }
+        // Metodo para limpiar los datos mostrados en los controles
+        private void Limpiar() {
+            // Text: propiedad de los controles de tipo string que  obtiene o establece el texto del control
+            txtApellido.Text = string.Empty;
+            txtNombres.Text = string.Empty;
+            txtDocumento.Text = string.Empty;
+
+            //SelectedIndex: propiedad del ComboBox de tipo int que obtiene o establece el indice del elemento seleccionado
+            // le pasamos el -1 para indicar que no hay ningun elemento seleccionado (los comboBox despliegan una lista)
+            cboTipoDocumento.SelectedIndex = -1;
+            cboEstadoCivil.SelectedIndex = -1;
+
+            //Checked: propiedad de tipo bool (true)indica que la casilla de verificacion esta activada (false)esta desactivado
+            rbtMasculino.Checked = false;
+            rbtFemenino.Checked = false;
+            chkFallecio.Checked = false;
+        }
+
+        // Evento que tiene lugar cuando se hace click en el Button Nuevo
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            nuevo = true; // Indicamos que ingresamos nuevas Personas
+            Habilitar(true); // para habilitar controles necesarios para ingresar datos
+            Limpiar();  //para limpiar los controles
+            txtApellido.Focus(); //hace que el cursor se pare en el control
+        }
+
+        // Evento que tiene lugar cuando se hace click en el Button Editar
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            
+            Habilitar(true); // para habilitar controles necesarios para editar datos
+            txtDocumento.Enabled = false; //falso para evitar error de integridad de datos por PK en la base de datos
+            txtApellido.Focus();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+
+        }
+        // Cancelar ya sea para nuevo o edicion 
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+            Habilitar(false);
+            nuevo = false;
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            if (nuevo) //(nuevo == true) es equivalente
+            {
+                //VALIDAR QUE NO EXISTA LA PK !!!(SI NO ES AUTOINCREMENTAL / IDENTITY)
+                /// Creamos una Persona y le seteamos las propiedades
+                Persona p = new Persona();
+                p.Apellido = txtApellido.Text;
+                p.Nombres = txtNombres.Text;
+                p.Documento = Convert.ToInt32(txtDocumento.Text);
+                /// tomamos el Value de los ComboBox, no el indice
+                p.TipoDocumento = Convert.ToInt32(cboTipoDocumento.SelectedValue);
+                p.EstadoCivil = Convert.ToInt32(cboEstadoCivil.SelectedValue);
+                p.Fallecio = chkFallecio.Checked;
+                if (rbtFemenino.Checked)
+                    p.Sexo = 1;               
+                else 
+                    p.Sexo = 2;
+                p.FechaNacimiento = dtpFechaNacimiento.Value;
+                // Insert con sentencia SQL Tradicional
+                string sqlInsert = "INSERT INTO personas VALUES('" + p.Apellido + "','"
+                                                                   + p.Nombres + "',"
+                                                                   + p.TipoDocumento +","
+                                                                   + p.Documento + ","
+                                                                   + p.EstadoCivil + ","
+                                                                   + p.Sexo + ",'"
+                                                                   + p.Fallecio +"','"
+                                                                   + p.FechaNacimiento +"')";
+
+                conexion.ConnectionString = @"Data Source=.\sqlexpress;Initial Catalog=TUPPI;Integrated Security=True";
+                conexion.Open();
+
+                comando.Connection = conexion;
+                comando.CommandType = CommandType.Text;
+                comando.CommandText = sqlInsert;
+                comando.ExecuteNonQuery();  /// Esto es para los Insert, Delete, Update no devueve tablas. Devuelve el numero de filas afectadas
+                                            /// Lo único que cambiaria serian las sentencias el CommandText
+                conexion.Close();
+
+                // Inster utilizando parametros (clase Parameters)
+            }
+            else { 
+                // Update
+            } 
+            Habilitar(false);
+            nuevo = false;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Desea abandonar la aplicación", "SALIR",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question, 
+                MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                this.Close();   // cierro el form
+            }
+        }
+    }
+}
